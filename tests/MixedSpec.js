3d7b9c03c3be4372c0a91fb60d126f976945fb1f
Sebastian Mayr <sebmaster16@gmail.com>
2013-05-18 02:12:10 +0200

    Add new tests, cleanup

---
+++ tests/MixedSpec.js
@@ -0,0 +1,8 @@
+﻿var XLSX = require('../');
+var testCommon = require('./Common.js');
+
+var file = 'mixed_sheets.xlsx';
+
+describe(file, function () {
+	testCommon(file);
+});
---
+++ tests/NamedRangesSpec.js
@@ -0,0 +1,8 @@
+﻿var XLSX = require('../');
+var testCommon = require('./Common.js');
+
+var file = 'named_ranges_2011.xlsx';
+
+describe(file, function () {
+	testCommon(file);
+});
