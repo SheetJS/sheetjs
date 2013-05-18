45c02aa71cb4296b369ce54cd8c2d80c3d8f3f51
Sebastian Mayr <sebmaster16@gmail.com>
2013-05-18 16:21:27 +0200

    Add tests for the remaining files

---
+++ tests/FormulaSpec.js
@@ -0,0 +1,9 @@
+﻿var XLSX = require('../');
+var testCommon = require('./Common.js');
+
+var file = 'formula_stress_test.xlsx';
+
+describe(file, function () {
+	// Opening the file currently crashes node
+	//testCommon(file);
+});
