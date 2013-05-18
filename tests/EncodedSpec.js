051fd0af648c9b59895774c00eb5ae23b683a8e6
Sebastian Mayr <sebmaster16@gmail.com>
2013-05-18 15:02:31 +0200

    Factor common tests in a common file

--- tests/EncodedSpec.js
+++ tests/EncodedSpec.js
@@ -1,20 +1,8 @@
 ﻿var XLSX = require('../');
+var testCommon = require('./Common.js');
 
-describe('חישוב_נקודות_זיכוי.xlsx', function () {
-	it('should be able to open workbook', function () {
-		var xlsx = XLSX.readFile('tests/files/חישוב_נקודות_זיכוי.xlsx');
-		expect(xlsx).toBeTruthy();
-		expect(xlsx).toEqual(jasmine.any(Object));
-	});
+var file = 'חישוב_נקודות_זיכוי.xlsx';
 
-	it('should define all api properties correctly', function () {
-		var xlsx = XLSX.readFile('tests/files/חישוב_נקודות_זיכוי.xlsx');
-		expect(xlsx.Workbook).toEqual(jasmine.any(Object));
-		expect(xlsx.Props).toBeDefined();
-		expect(xlsx.Deps).toBeDefined();
-		expect(xlsx.Sheets).toEqual(jasmine.any(Object));
-		expect(xlsx.SheetNames).toEqual(jasmine.any(Array));
-		expect(xlsx.Strings).toBeDefined();
-		expect(xlsx.Styles).toBeDefined();
-	});
+describe(file, function () {
+	testCommon(file);
 });
