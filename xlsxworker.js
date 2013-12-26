7c5bc5b20d2186adef2610d6750e37d01ece2c88
SheetJS <dev@sheetjs.com>
2013-12-26 00:38:44 -0500

    default to readAsBinaryString

--- xlsxworker.js
+++ xlsxworker.js
@@ -1,3 +1,4 @@
+/* xlsx.js (C) 2013 SheetJS -- http://sheetjs.com */
 importScripts('jszip.js');
 importScripts('xlsx.js');
 postMessage({t:"ready"});
@@ -5,7 +6,8 @@ postMessage({t:"ready"});
 onmessage = function (oEvent) {
   var v;
   try {
-    v = XLSX.read(oEvent.data, {type: 'base64'});
+    //postMessage({t:'e', d:oEvent.data.b});
+    v = XLSX.read(oEvent.data.d, {type: oEvent.data.b ? 'binary': 'base64'});
   } catch(e) { postMessage({t:"e",d:e.stack}); }
   postMessage({t:"xlsx", d:JSON.stringify(v)});
 };
