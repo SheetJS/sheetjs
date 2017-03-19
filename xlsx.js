ea7a951506b2f1686288903c859371f9c5f63069
František Žiačik <ziacik@gmail.com>
2017-03-20 00:46:41 +0100

    better dup check (fixes #316)

--- xlsx.js
+++ xlsx.js
@@ -1353,7 +1353,7 @@ function str2cc(str) {
 
 function dup(o) {
 	if(typeof JSON != 'undefined') return JSON.parse(JSON.stringify(o));
-	if(typeof o != 'object' || !o) return o;
+	if(typeof o != 'object' || o == null) return o;
 	var out = {};
 	for(var k in o) if(o.hasOwnProperty(k)) out[k] = dup(o[k]);
 	return out;
