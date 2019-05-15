4109caeadd95baca58c7c2481aebf2014ae462bd
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2019-05-15 20:40:43 +0200

    to_csv skipHidden corner case (fixes #1508)

--- bits/97_node.js
+++ bits/97_node.js
@@ -18,7 +18,6 @@ if(has_buf && typeof require != 'undefined') (function() {
 		var BOM = false;
 		stream._read = function() {
 			if(!BOM) { BOM = true; return stream.push("\uFEFF"); }
-			if(R > r.e.r) return stream.push(null);
 			while(R <= r.e.r) {
 				++R;
 				if ((rowinfo[R-1]||{}).hidden) continue;
@@ -29,6 +28,7 @@ if(has_buf && typeof require != 'undefined') (function() {
 					break;
 				}
 			}
+			if(R > r.e.r) return stream.push(null);
 		};
 		return stream;
 	};
