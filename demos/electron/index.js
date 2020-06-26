6d3fac403c6e94edbe2e466da0315d6c107f0074
Srijon Saha <srijon365@gmail.com>
2020-06-26 12:46:25 -0700

    Remove Google Analytics code block
    [ci skip]

--- demos/electron/index.js
+++ demos/electron/index.js
@@ -19,16 +19,6 @@ var process_wb = (function() {
 	};
 })();
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-36810333-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'https://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 var do_file = (function() {
 	return function do_file(files) {
 		var f = files[0];
