90fda3e73911bde5d56dd7819db7a9b9373e9436
Srijon Saha <srijon365@gmail.com>
2020-06-26 01:13:41 -0700

    Add security policy to remove Electron security warnings
    [ci skip]

--- demos/electron/index.js
+++ demos/electron/index.js
@@ -1,4 +1,4 @@
-/* xlsx.js (C) 2013-present SheetJS -- http://sheetjs.com */
+/* xlsx.js (C) 2013-present SheetJS -- https://sheetjs.com */
 /* global Uint8Array, console */
 /* exported export_xlsx */
 /* eslint no-use-before-define:0 */
@@ -25,7 +25,7 @@ _gaq.push(['_trackPageview']);
 
 (function() {
 	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'https://www') + '.google-analytics.com/ga.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 
