a7405c4a55ee361dabaa3422e2278c5c5a8c071c
notchris <mail@chrismcgrane.com>
2021-11-03 00:44:45 -0400

    Fixed RequireJS export name [ci skip]

--- bits/99_footer.js
+++ bits/99_footer.js
@@ -3,7 +3,7 @@
 /*:: declare var define:any; */
 if(typeof exports !== 'undefined') make_xlsx_lib(exports);
 else if(typeof module !== 'undefined' && module.exports) make_xlsx_lib(module.exports);
-else if(typeof define === 'function' && define.amd) define('xlsx-dist', function() { if(!XLSX.version) make_xlsx_lib(XLSX); return XLSX; });
+else if(typeof define === 'function' && define.amd) define('xlsx', function() { if(!XLSX.version) make_xlsx_lib(XLSX); return XLSX; });
 else make_xlsx_lib(XLSX);
 /* NOTE: the following extra line is needed for "Lightning Locker Service" */
 if(typeof window !== 'undefined' && !window.XLSX) window.XLSX = XLSX;
