199373e9187d0ce7b49e97115ab61164fb02d558
wangkai53 <wangkai53@sfmail.sf-express.com>
2022-08-17 09:52:36 +0800

    feat(98_esmxport): add esm default export

--- misc/98_esmxport.js
+++ misc/98_esmxport.js
@@ -18,3 +18,22 @@ export {
 	SSF,
 	CFB
 };
+export default {
+	parse_xlscfb,
+	parse_zip,
+	readSync as read,
+	readFileSync as readFile,
+	readFileSync,
+	writeSync as write,
+	writeFileSync as writeFile,
+	writeFileSync,
+	writeFileAsync,
+	writeSyncXLSX as writeXLSX,
+	writeFileSyncXLSX as writeFileXLSX,
+	utils,
+	set_fs,
+	set_cptable,
+	__stream as stream,
+	SSF,
+	CFB
+}
